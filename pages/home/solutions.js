4bf2eb5400757d7fc7c0202c515e38d10ba99e9f
chandranfullstack <chandran@fulgid.in>
2023-06-28 10:00:37 +0530

    err messages

--- pages/home/solutions.js
+++ pages/home/solutions.js
@@ -1,18 +1,12 @@
 import {ContentProvider} from "../api/builder"
 import { loadAllData} from "../api/server/index"
-import axios from "axios"
-
 
 const Solutions=({data})=>{
    return ContentProvider({data})
 }
 export default Solutions
 
-export const getServerSideProps=async()=>{
-   const data=await axios.get("https://builder-server.onrender.com/page")
-   console.log(data,"data")
+export const getStaticProps=async()=>{
+   const data=await loadAllData()
    return {props:{data}}
 }
-
-
-

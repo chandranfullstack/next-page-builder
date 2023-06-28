01b17bc7f5c93a0db15e8e37250218db84a9e4ec
chandranfullstack <chandran@fulgid.in>
2023-06-29 00:43:39 +0530

    r

--- pages/[dynamic].js
+++ pages/[dynamic].js
@@ -13,11 +13,11 @@ export default Dynamic
        paths: [
          {
            params: {
-             dynamic: '/'
+             dynamic: 'home'
            },
          }, 
       ],
-       fallback:false, 
+       fallback:"blocking", 
      }
    }
 

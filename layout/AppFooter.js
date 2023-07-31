5b919b1a633a1b1402b37dd4f1fd7a42dd08ab51
chandranfullstack <hackchandran@gmail.com>
2023-07-31 03:51:21 +0000

    changes image and section

--- layout/AppFooter.js
+++ layout/AppFooter.js
@@ -7,7 +7,6 @@ const AppFooter = () => {
 
     return (
         <div className="layout-footer">
-            Provide
             {/* <Image src={`/builderFooter/logo-dark.svg.png`} alt="Logo" height={20} width={40} className="mr-2" /> */}
             by
             <Image src={`/builder/site-logo.png`} alt="Logo" height={20} width={160} className="mr-2" />

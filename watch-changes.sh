8782fcb2064285b4bbbdf8288735261fd653f738
chandranfullstack <hackchandran@gmail.com>
2023-08-14 04:51:19 +0000

    changes from server

--- watch-changes.sh
+++ watch-changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Watch for file changes and trigger build
-chokidar "pages/**/*" "components/**/*" -c "npm run build && pm2 restart next-page-builder"
+chokidar "pages/**/*" "components/**/*" "public/data/**" -c "npm run build && pm2 restart next-page-builder" --initial
